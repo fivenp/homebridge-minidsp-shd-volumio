58754ae862172a0002f97b542c5e56967bba67a7
fivenp <mh@fivenp.com>
2022-06-17 22:57:09 +0200

    init
    Signed-off-by: fivenp <mh@fivenp.com>

---
+++ src/tvAccessory.ts
@@ -0,0 +1,229 @@
+import {
+  CharacteristicValue,
+  Logger,
+  PlatformAccessory,
+  Service,
+} from "homebridge";
+import fetch from "node-fetch";
+import io from "socket.io-client";
+
+import { SHDPlatform } from "./platform";
+
+export class SHDTvAccessory {
+  private tvService: Service;
+
+  private state = {
+    Active: true,
+    ActiveIdentifier: 0,
+    APIUrl: this.platform.config.api,
+  };
+
+  private deviceName: string;
+  private log: Logger;
+  private socket;
+
+  private sources = [
+    {
+      name: "TOSLINK",
+      type: "2",
+      activeValue: "1",
+      remoteId: "1",
+    },
+    {
+      name: "RCA",
+      type: "2",
+      activeValue: "2",
+      remoteId: "4",
+    },
+    {
+      name: "STREAMING",
+      type: "2",
+      activeValue: "3",
+      remoteId: "99",
+    },
+    {
+      name: "SPDIF",
+      type: "1",
+      activeValue: "4",
+      remoteId: "2",
+    },
+    {
+      name: "AES-EBU",
+      type: "3",
+      activeValue: "5",
+      remoteId: "3",
+    },
+    {
+      name: "XLR",
+      type: "3",
+      activeValue: "6",
+      remoteId: "5",
+    },
+    {
+      name: "USB",
+      type: "3",
+      activeValue: "7",
+      remoteId: "6",
+    },
+  ];
+
+  constructor(
+    private readonly platform: SHDPlatform,
+    private readonly accessory: PlatformAccessory
+  ) {
+    const Characteristic = this.platform.Characteristic;
+
+    this.log = platform.log;
+    this.deviceName = "SHD Input";
+    this.socket = io.connect(`${this.state.APIUrl}:3000`);
+
+    // Get initial state and listen for updates
+    this.socket.on("pushState", this.updateFromSocket.bind(this));
+    this.socket.emit("getState", "");
+
+    this.accessory
+      .getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(Characteristic.Manufacturer, "MiniDSP")
+      .setCharacteristic(Characteristic.Model, "SHD")
+      .setCharacteristic(Characteristic.SerialNumber, "---------");
+
+    this.tvService =
+      this.accessory.getService(this.platform.Service.Television) ||
+      this.accessory.addService(this.platform.Service.Television);
+
+    this.tvService
+      .setCharacteristic(this.platform.Characteristic.Name, this.deviceName)
+      .setCharacteristic(Characteristic.ConfiguredName, this.deviceName)
+      .setCharacteristic(
+        Characteristic.SleepDiscoveryMode,
+        Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE
+      );
+
+    this.tvService.setCharacteristic(
+      Characteristic.ActiveIdentifier,
+      this.state.ActiveIdentifier
+    );
+
+    this.tvService
+      .getCharacteristic(Characteristic.Active)
+      .onSet(this.setActive.bind(this))
+      .onGet(this.getActive.bind(this));
+
+    this.tvService
+      .getCharacteristic(Characteristic.ActiveIdentifier)
+      .onSet(this.setActiveIdentifier.bind(this))
+      .onGet(this.getActiveIdentifier.bind(this));
+
+    for (const i in this.sources) {
+      const source = this.sources[i];
+      const index = parseInt(i) + 1;
+      const serviceName = `INPUT+${index}`;
+
+      const inputService =
+        this.accessory.getService(serviceName) ||
+        this.accessory.addService(
+          this.platform.Service.InputSource,
+          serviceName,
+          `${source.name}`
+        );
+
+      this.log.info("[%s] Adding inputService ->", this.deviceName, source);
+
+      inputService
+        .setCharacteristic(Characteristic.Identifier, parseInt(i) + 1)
+        .setCharacteristic(Characteristic.ConfiguredName, source.name)
+        .setCharacteristic(
+          Characteristic.IsConfigured,
+          Characteristic.IsConfigured.CONFIGURED
+        )
+        .setCharacteristic(
+          Characteristic.InputSourceType,
+          source.type || Characteristic.InputSourceType.TUNER
+        );
+
+      this.tvService.addLinkedService(inputService);
+    }
+  }
+
+  a;
+
+  async setActive(value: CharacteristicValue) {
+    try {
+      if (value === 1) {
+        this.log.info("[%s] Power -> [on]", this.deviceName, true);
+        this.state.Active = true;
+      } else {
+        this.log.info("[%s] Power -> [off]", this.deviceName, false);
+        this.state.Active = false;
+      }
+    } catch (error) {
+      this.log.error("[%s] Error setting state:", this.deviceName, error);
+    }
+  }
+
+  updateFromSocket(data) {
+    this.log.debug("[%s] Got new state from socket...", this.deviceName);
+    this.getActiveIdentifier();
+  }
+
+  async getActive(): Promise<CharacteristicValue> {
+    this.log.debug(
+      "[%s] Getting current state..",
+      this.deviceName,
+      this.state.Active
+    );
+    return this.state.Active;
+  }
+
+  async setActiveIdentifier(value: CharacteristicValue) {
+    const sourceIndex = parseInt(value as string) - 1;
+    const source = this.sources[sourceIndex];
+
+    this.log.info("[%s] Selecting input -> [%s]", this.deviceName, source.name);
+
+    try {
+      this.socket.emit("browseLibrary", { uri: `inputs/id/${value}` });
+      this.state.ActiveIdentifier = value as number;
+      this.tvService
+        .getCharacteristic(this.platform.Characteristic.ActiveIdentifier)
+        .updateValue(value);
+    } catch (error) {
+      this.log.error("[%s] Error selecting input:", this.deviceName, error);
+    }
+  }
+
+  async getActiveIdentifier(): Promise<CharacteristicValue> {
+    const response = await fetch(`${this.state.APIUrl}/api/v1/getState`);
+    const data = await response.json();
+    let currentlyActive = "3";
+
+    this.sources.forEach((source) => {
+      if (data.title === source.name) {
+        currentlyActive = source.activeValue;
+      }
+    });
+
+    this.log.debug(
+      "[%s] Getting active input..",
+      this.deviceName,
+      this.state.ActiveIdentifier,
+      data.title,
+      currentlyActive
+    );
+
+    if (parseInt(currentlyActive) !== this.state.ActiveIdentifier) {
+      this.log.debug(
+        "[%s] Switch active input from... to...",
+        this.deviceName,
+        this.state.ActiveIdentifier,
+        currentlyActive
+      );
+      this.state.ActiveIdentifier = parseInt(currentlyActive);
+      this.tvService
+        .getCharacteristic(this.platform.Characteristic.ActiveIdentifier)
+        .updateValue(currentlyActive);
+    }
+
+    return this.state.ActiveIdentifier;
+  }
+}
---
+++ src/volumeAccessory.ts
@@ -0,0 +1,140 @@
+import {
+  Logger,
+  Service,
+  PlatformAccessory,
+  CharacteristicValue,
+} from "homebridge";
+import fetch from "node-fetch";
+import io from "socket.io-client";
+
+import { SHDPlatform } from "./platform";
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class SHDVolumeAccessory {
+  private bulbService: Service;
+  private deviceName: string;
+  private log: Logger;
+
+  /**
+   * These are just used to create a working example
+   * You should implement your own code to track the state of your accessory
+   */
+  private state = {
+    On: true,
+    APIUrl: this.platform.config.api,
+    Volume: this.platform.config.defaultVolume,
+  };
+
+  private socket;
+
+  constructor(
+    private readonly platform: SHDPlatform,
+    private readonly accessory: PlatformAccessory
+  ) {
+    this.log = platform.log;
+    this.deviceName = "SHD Volume";
+    this.socket = io.connect(`${this.state.APIUrl}:3000`);
+
+    // Get initial state and listen for updates
+    this.socket.on("pushState", this.updateFromSocket.bind(this));
+    this.socket.emit("getState", "");
+
+    // set accessory information
+    this.accessory
+      .getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, "MiniDSP")
+      .setCharacteristic(this.platform.Characteristic.Model, "SHD")
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, "--------");
+
+    // get the LightBulb service if it exists, otherwise create a new LightBulb service
+    // you can create multiple services for each accessory
+    this.bulbService =
+      this.accessory.getService(this.platform.Service.Lightbulb) ||
+      this.accessory.addService(this.platform.Service.Lightbulb);
+
+    // set the service name, this is what is displayed as the default name on the Home app
+    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.bulbService.setCharacteristic(
+      this.platform.Characteristic.Name,
+      accessory.context.device.exampleDisplayName
+    );
+
+    // each service must implement at-minimum the "required characteristics" for the given service type
+    // see https://developers.homebridge.io/#/service/Lightbulb
+
+    // register handlers for the On/Off Characteristic
+    this.bulbService
+      .getCharacteristic(this.platform.Characteristic.On)
+      .onSet(this.setOn.bind(this)) // SET - bind to the `setOn` method below
+      .onGet(this.getOn.bind(this)); // GET - bind to the `getOn` method below
+
+    // register handlers for the Brightness Characteristic
+    this.bulbService
+      .getCharacteristic(this.platform.Characteristic.Brightness)
+      .onSet(this.setVolume.bind(this)); // SET - bind to the 'setVolume` method below
+  }
+
+  /**
+   * Handle "SET" requests from HomeKit
+   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
+   */
+  async setOn(value: CharacteristicValue) {
+    this.state.On = true;
+  }
+
+  /**
+   * Handle the "GET" requests from HomeKit
+   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
+   *
+   * GET requests should return as fast as possbile. A long delay here will result in
+   * HomeKit being unresponsive and a bad user experience in general.
+   *
+   * If your device takes time to respond you should update the status of your device
+   * asynchronously instead using the `updateCharacteristic` method instead.
+
+   * @example
+   * this.bulbService.updateCharacteristic(this.platform.Characteristic.On, true)
+   */
+  async getOn(): Promise<CharacteristicValue> {
+    const isOn = true;
+    const currentVolume = await this.getVolume();
+
+    this.bulbService
+      .getCharacteristic(this.platform.Characteristic.Brightness)
+      .updateValue(currentVolume);
+
+    this.log.debug(
+      "[%s] Get Initial Volume ->",
+      this.deviceName,
+      currentVolume
+    );
+    return isOn;
+  }
+
+  /**
+   * Handle "SET" requests from HomeKit
+   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
+   */
+  async setVolume(value: CharacteristicValue) {
+    // implement your own code to set the brightness
+    this.state.Volume = value as number;
+    const newVolume = this.state.Volume;
+    this.socket.emit("volume", newVolume);
+    this.log.info("[%s] Set Volume -> ", this.deviceName, newVolume);
+  }
+
+  async getVolume() {
+    const response = await fetch(`${this.state.APIUrl}/api/v1/getState`);
+    const data = await response.json();
+    return data.volume;
+  }
+
+  updateFromSocket(data) {
+    this.log.debug("[%s] Got new state from socket...", this.deviceName);
+    this.getOn();
+  }
+}
